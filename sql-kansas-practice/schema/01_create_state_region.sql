f4d59f1c4e2d5deee18b98f54c59035569ee947a
profshai <profshaiy@gmail.com>
2026-04-25 01:35:42 -0500

    remove large/unwanted files from repo

--- sql-kansas-practice/schema/01_create_state_region.sql
+++
@@ -1,9 +0,0 @@
--- 01_create_state_region.sql
--- Optional lookup table for region-level analysis
-
-DROP TABLE IF EXISTS state_region;
-
-CREATE TABLE state_region (
-    state TEXT,
-    region TEXT
-);
